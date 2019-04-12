a5677c55dd1ac1a02541790cb6407160b1bfada0
Najib Adan <najibadan@airmail.cc>
2019-04-12 06:48:02 +0300

    fixes

--- ch02/assignment/proxy_server/proxy_server.py
+++ ch02/assignment/proxy_server/proxy_server.py
@@ -2,7 +2,7 @@ from socket import *
 import sys
 
 if len(sys.argv) <= 1:
-	print 'Usage : "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server'
+	print('Usage : "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server')
 	sys.exit(2)
 
 # Create a server socket, bind it to a port and start listening
@@ -12,38 +12,38 @@ tcpSerSock.listen(1)
 
 while 1:
 	# Start receiving data from the client
-	print '\nReady to serve...'
+	print('\nReady to serve...')
 	tcpCliSock, addr = tcpSerSock.accept()
-	print 'Received a connection from:', addr
+	print('Received a connection from:', addr)
 	message = tcpCliSock.recv(2048)
-	print message
+	print(message)
 
 	# Extract the filename from the given message
-	print message.split()[1]
+	print (message.split()[1])
 	filename = message.split()[1].partition("/")[2]
-	print filename
-	fileExist = "false"
+	print (filename)
+	fileExists = False
 	filetouse = "/" + filename
-	print filetouse
+	print(filetouse)
 	try:
 		# Check wether the file exist in the cache
 		f = open(filetouse[1:], "r")
 		outputdata = f.readlines()
-		fileExist = "true"
+		fileExists = True
 		# ProxyServer finds a cache hit and generates a response message
 		tcpCliSock.send("HTTP/1.0 200 OK\r\n")
 		tcpCliSock.send("Content-Type:text/html\r\n")
 		tcpCliSock.send("\r\n")
 		for line in outputdata:
 			tcpCliSock.send(line + "\r\n")
-		print 'Read from cache'
+		print('Read from cache')
 	# Error handling for file not found in cache
 	except IOError:
-		if fileExist == "false":
+		if fileExists == False:
 			# Create a socket on the proxyserver
 			c = socket(AF_INET, SOCK_STREAM)
 			hostn = filename.replace("www.","",1)
-			print hostn
+			print(hostn)
 			try:
 				# Connect to the socket to port 80
 				c.connect((hostn, 80))
@@ -58,7 +58,7 @@ while 1:
 				tmpFile.write(data)
 				tcpCliSock.send(data)
 			except:
-				print "Illegal request"
+				print("Illegal request")
 		else:
 			# HTTP response message for file not found
 			tcpCliSock.send("HTTP/1.1 404 Not Found\r\n")
