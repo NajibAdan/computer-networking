c4e90f97e4edece11afac1dd77a582f4609cd23f
Najib Adan <najibadan@airmail.cc>
2019-04-25 06:49:21 +0300

    Implemented checksum

--- ch04/traceroute/traceroute.py
+++ ch04/traceroute/traceroute.py
@@ -14,8 +14,25 @@ TRIES = 2
 # request packet, which is exactly what we had used in the ICMP ping exercise.
 # We shall use the same packet that we built in the Ping exercise
 def checksum(str):
-# In this function we make the checksum of our packet
-# hint: see icmpPing lab
+    str_ = bytearray(str_)
+    csum = 0
+    countTo = (len(str_) // 2) * 2
+
+    for count in range(0, countTo, 2):
+        thisVal = str_[count+1] * 256 + str_[count]
+        csum = csum + thisVal
+        csum = csum & 0xffffffff
+
+    if countTo < len(str_):
+        csum = csum + str_[-1]
+        csum = csum & 0xffffffff
+
+    csum = (csum >> 16) + (csum & 0xffff)
+    csum = csum + (csum >> 16)
+    answer = ~csum
+    answer = answer & 0xffff
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+    return answer
 
 def build_packet():
 # In the sendOnePing() method of the ICMP Ping exercise ,firstly the header of our
@@ -27,6 +44,7 @@ def build_packet():
 # So the function ending should look like this
 # packet = header + data
 # return packet
+
 def get_route(hostname):
     timeLeft = TIMEOUT
     for ttl in xrange(1,MAX_HOPS):
